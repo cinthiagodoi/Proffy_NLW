05de94b5745327e5cc33f4dbf04a0f76f0b18315
Cinthia Godoi <cinthiagodoi@hotmail.com>
2020-08-07 22:45:28 +0100

    day five

--- mobile/src/pages/Favorites/styles.ts
+++ mobile/src/pages/Favorites/styles.ts
@@ -1,5 +1,14 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f0f0f7",
+  },
+
+  teacherList: {
+    marginTop: -40,
+  },
+});
 
 export default styles;
